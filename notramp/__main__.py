974d697088853b61d5392b8d85e1239672f89c9c
simakro <74452164+simakro@users.noreply.github.com>
2025-01-24 12:06:53 +0100

    chk if pkg_path in sys.path

--- notramp/__main__.py
+++ notramp/__main__.py
@@ -2,5 +2,7 @@ import os, sys
 import notramp.notramp_main
 
 if __name__ == "__main__":
-    sys.path.append(os.path.split(__file__)[0])
+    pkg_path = os.path.split(__file__)[0]
+    if pkg_path not in sys.path:
+        sys.path.append(pkg_path)
     notramp.notramp_main.run_notramp()
